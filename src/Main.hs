2b61cceeaff1b0a52cfafaa0b109ed14ed9e2cb8
Florian Klein <florian.klein@free.fr>
2020-02-22 22:32:07 +0100

    wip

--- src/Main.hs
+++ src/Main.hs
@@ -13,6 +13,7 @@ import Network.Wai.EventSource (ServerEvent(..), eventSourceAppIO)
 import Network.Wai.Handler.Warp (runEnv)
 import Hasql.Session (Session)
 import Hasql.Statement (Statement)
+import Hasql.Notification
 import qualified Hasql.Session as Session
 import qualified Hasql.Connection as Connection
 import qualified Hasql.TH as TH
@@ -29,11 +30,11 @@ import System.Log.FastLogger ( ToLogStr(..)
 
 type API = ReqBody '[JSON] Message :> Post '[JSON] NoContent
       :<|> Get '[JSON] (Vector Text)
-      -- :<|> "sse"
-      --      :> Capture "topic" Text
-      --      :> QueryParam "since" Text
-      --      :> Header "Last-Event-Id" Text
-      --      :> Raw
+      :<|> "sse"
+           :> Capture "topic" Text
+           :> QueryParam "since" Text
+           :> Header "Last-Event-Id" Text
+           :> Raw
 
 api :: Proxy API
 api = Proxy
@@ -68,8 +69,7 @@ initSchema = [TH.uncheckedSql|
 type AppM = ReaderT AppCtx Handler
 
 server :: ServerT API AppM
-server = postMessage :<|> getMessages
-              -- :<|> sse
+server = postMessage :<|> getMessages :<|> sse
     where
         postMessage :: Message -> AppM NoContent
         postMessage msg = do
@@ -92,27 +92,32 @@ server = postMessage :<|> getMessages
             case result of
                 Right messages -> pure messages
                 Left e -> parseUsageError e
+            where
+                selectMessages :: Statement () (Vector (Text))
+                selectMessages = [TH.vectorStatement|select content::text from message|]
+
+        sse topic since lastEventId = Tagged $ \req respond -> do
+            putStrLn $ show since
+            putStrLn $ show lastEventId
+            -- --_ <- withResource pool $ \conn -> do
+            -- --    notifications <- query conn "select content from message where at <= now()" ()
+            -- --    eventSourceAppIO (since notifications) req respond
+
+            eventSourceAppIO (listen topic) req respond
+            where
+                listen :: Text -> IO ServerEvent
+                listen topic = do
+                    pool <- asks pool
+                    result <- liftIO $ use pool $ Session.sql [TH.uncheckedSql|listen "test"|]
+                    notification <- getNotification $ use pool
+                    case result of
+                        Right notification -> event notification
+                        Left e -> error "nope"
+
+                    where
+                        event notification' = return $ ServerEvent (Just $ string8 $ show $ notificationChannel notification')
+                                                         Nothing
+                                                         [string8 $ show $ notificationData notification']
 
         parseUsageError e = throwError err500 {errBody = pack $ show e}
-
-selectMessages :: Statement () (Vector (Text))
-selectMessages = [TH.vectorStatement|select content::text from message|]
-
-        --sse topic since lastEventId = Tagged $ \req respond -> do
-        --    putStrLn $ show since
-        --    putStrLn $ show lastEventId
-        --    --_ <- withResource pool $ \conn -> do
-        --    --    notifications <- query conn "select content from message where at <= now()" ()
-        --    --    eventSourceAppIO (since notifications) req respond
-
-        --    eventSourceAppIO (listen pool topic) req respond
-
-        --listen :: Pool -> Text -> IO ServerEvent
-        --listen pool' topic =
-        --    withResource pool' $ \conn -> do
-        --        _ <- execute conn "listen ?" (Only $ Identifier topic)
-        --        notification <- getNotification conn
-        --        return $ ServerEvent (Just $ string8 $ show $ notificationChannel notification)
-        --                             Nothing
-        --                             [string8 $ show $ notificationData notification]
 
